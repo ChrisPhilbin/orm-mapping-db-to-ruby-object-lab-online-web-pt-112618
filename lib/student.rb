490952fba9002eec8b90cb38f66ce362ed8ce3e3
Chris Philbin <chris@chrisphilbin.net>
2019-02-14 02:32:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,6 +60,17 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
+  def self.all_students_in_grade_X(number)
+    sql = <<-SQL
+        SELECT * FROM students
+        WHERE grade = ?
+      SQL
+
+    DB[:conn].execute(sql, number).map do |row|
+      self.new_from_db
+    end
+  end
+
   def self.first_student_in_grade_10
     sql = <<-SQL
         SELECT * FROM students
